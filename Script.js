0f228a40e2acbe4554291a4377496a5a658b1b4d
deepikajada123-byte <deepikajada123@gmail.com>
2026-07-22 15:08:01 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+const text = "AI & Machine Learning Enthusiast";
+let i = 0;
+
+function typing() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 100);
+    }
+}
+
+typing();
